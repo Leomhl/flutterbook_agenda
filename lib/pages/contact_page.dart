2d920140b2bf143e3b07230d2ac7ce59d55cbc76
Leonardo <leonardomarinho.nf@gmail.com>
2020-09-02 09:43:53 -0300

    Melhorias

--- lib/pages/contact_page.dart
+++ lib/pages/contact_page.dart
@@ -20,7 +20,7 @@ class _ContactPageState extends State<ContactPage> {
 
   final _nameFocus = FocusNode();
 
-  bool   = false;
+  bool _userEdited = false;
 
   Contact _editedContact;
 
@@ -64,7 +64,7 @@ class _ContactPageState extends State<ContactPage> {
           padding: EdgeInsets.all(10.0),
           child: Column(
             children: <Widget>[
-              pc(
+              Container(
                 width: 140.0,
                 height: 140.0,
                 decoration: BoxDecoration(
